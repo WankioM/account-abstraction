f2f3d47a8639bbf843f36a852af439b42c607894
WankioM <tracywankio29@gmail.com>
2025-08-08 17:26:48 +0300

    validate ops

--- src/MinimalAccount.sol
+++ src/MinimalAccount.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-contract MinimalAccount {
+import {IAccount} from "account-abstraction/interfaces/IAccount.sol";
+import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+import Ownable from "@openzeppelin/contracts/access/Ownable.sol"; 
+
+contract MinimalAccount is IAccount {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    constructor() Ownable(msg.sender) {}
+    
+    // A signature is valid if its the Minimal Account owner
 
-    function increment() public {
-        number++;
-    }
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData){
+        _validateSignature(userOp, userOpHash);
+    };
+
+    _validateSignature
 }
